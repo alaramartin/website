eedce5837abc64fb3b04b2b34d1b4b58273eac82
alaramartin <alara.martin@gmail.com>
2025-10-02 02:09:52 -0700

    changed the format and now it looks like modern art

--- app/components/LinksBar.tsx
+++ app/components/LinksBar.tsx
@@ -1,43 +1,43 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
+import {
+    GithubLogoIcon,
+    LinkedinLogoIcon,
+    WindowsLogoIcon,
+} from "@phosphor-icons/react/dist/ssr";
 
-const LinkedIcon = ({
-    href,
-    imageSrc,
-    alt,
-}: {
-    href: string;
-    imageSrc: string;
-    alt: string;
-}) => {
-    return (
-        <div className="px-2">
-            <Link href={href} target="_blank">
-                <Image src={imageSrc} alt={alt} width={20} height={20}></Image>
-            </Link>
-        </div>
-    );
-};
+const links: { name: string; href: string; icon: any }[] = [
+    {
+        name: "GitHub",
+        href: "https://github.com/alaramartin",
+        icon: GithubLogoIcon,
+    },
+    {
+        name: "LinkedIn",
+        href: "https://linkedin.com/in/alara-martin",
+        icon: LinkedinLogoIcon,
+    },
+    {
+        name: "VS Code Marketplace",
+        href: "https://marketplace.visualstudio.com/publishers/alarm",
+        icon: WindowsLogoIcon,
+    },
+];
 
 const LinksBar = () => {
     return (
-        <div className="border-2 border-red-200 bg-[#E2CFC5] rounded-4xl m-4 p-4 absolute bottom-0 flex flex-row shadow-xl/20 shadow-[#E2CFC5]">
-            <LinkedIcon
-                href="https://github.com/alaramartin"
-                imageSrc="/github.svg"
-                alt="GitHub"
-            ></LinkedIcon>
-            <LinkedIcon
-                href="https://linkedin.com/in/alara-martin"
-                imageSrc="/linkedin.svg"
-                alt="LinkedIn"
-            ></LinkedIcon>
-            <LinkedIcon
-                href="https://marketplace.visualstudio.com/publishers/alarm"
-                imageSrc="/microsoft.png"
-                alt="VS Code Marketplace"
-            ></LinkedIcon>
+        <div className="flex justify-center m-auto">
+            {links.map((link) => (
+                <div className="px-2" key={link.name}>
+                    <Link
+                        href={link.href}
+                        target="_blank"
+                        aria-label={link.name}
+                    >
+                        <link.icon size={32} />
+                    </Link>
+                </div>
+            ))}
         </div>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import LinksBar from "./components/LinksBar";
 
 export default function Home() {
     return (
-        <div
-            className={`flex h-screen items-center justify-center ${italiana.className} antialiased`}
-        >
-            <p className="text-5xl">ALARA MARTIN</p>
-            <LinksBar></LinksBar>
+        <div className={`h-screen ${italiana.className} antialiased relative`}>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <LinksBar></LinksBar>
+            </div>
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 pb-4 flex items-baseline cursor-default">
+                <p className="text-9xl whitespace-nowrap pr-5">ALARA</p>
+                <p className="text-7xl whitespace-nowrap text-[#d58789]">
+                    MARTIN
+                </p>
+            </div>
         </div>
     );
 }
