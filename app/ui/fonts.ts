1a555d3d4ab2ec9ec3bac33a4b8b22e9becffc4d
alaramartin <alara.martin@gmail.com>
2025-10-01 19:53:02 -0700

    why is this so difficult... put my name on the homescreen and chose colors

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Roboto, Italiana } from "next/font/google";
+
+export const italiana = Italiana({
+    subsets: ["latin"], weight: ['400']
+});
+
+export const roboto = Roboto({
+    subsets: ["latin"],
+});
