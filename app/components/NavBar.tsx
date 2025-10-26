051810c9c50f7f104ef56e570b583ad6e862766e
alaramartin <alara.martin@gmail.com>
2025-10-26 01:12:37 -0700

    changed navbar but idk if i like it

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,20 +1,50 @@
 import Link from "next/link";
+import {
+    HouseIcon,
+    AddressBookIcon,
+    FolderOpenIcon,
+    QuotesIcon,
+} from "@phosphor-icons/react/dist/ssr";
+
+const navLinks = [
+    { icon: HouseIcon, title: "Home", link: "/" },
+    { icon: FolderOpenIcon, title: "Projects", link: "/projects" },
+    { icon: QuotesIcon, title: "Blog", link: "/blog" },
+    { icon: AddressBookIcon, title: "Contact", link: "/contact" },
+];
 
 export default function NavBar() {
     return (
-        <div className="fixed top-0 left-0 px-8 pt-4 pb-2 gap-8 z-1000 bg-inherit w-full space-x-26">
-            <Link href="/" className="hover:underline">
-                Home
-            </Link>
-            <Link href="/projects" className="hover:underline">
-                Projects
-            </Link>
-            <Link href="/blog" className="hover:underline">
-                Blog
-            </Link>
-            <Link href="/contact" className="hover:underline">
-                Contact
-            </Link>
+        <div className="fixed top-0 left-0 w-full px-8 pt-4 pb-2 z-50 bg-inherit">
+            <div className="mx-auto flex items-center justify-start space-x-26">
+                {navLinks.map((navLink) => {
+                    const Icon = navLink.icon;
+                    return (
+                        <div
+                            key={navLink.link}
+                            className="relative flex items-center"
+                        >
+                            <Link
+                                href={navLink.link}
+                                className="peer inline-flex items-center p-2 rounded-md"
+                                aria-label={navLink.title}
+                            >
+                                <Icon size={24} />
+                            </Link>
+                            <p
+                                className="absolute left-full ml-2 top-1/2 -translate-y-1/2
+                                           opacity-0 pointer-events-none select-none
+                                           whitespace-nowrap underline
+                                           transition-opacity duration-200
+                                           peer-hover:opacity-95 peer-hover:pointer-events-auto"
+                                aria-hidden="true"
+                            >
+                                {navLink.title}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
